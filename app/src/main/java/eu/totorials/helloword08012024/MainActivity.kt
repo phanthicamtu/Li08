645ae734d73c05ccecb53e11182841bfb08727c6
Tu <tu@Tons-Mac-Air.local>
2024-03-01 17:19:10 +0700

    linear

---
+++ app/src/main/java/eu/totorials/helloword08012024/MainActivity.kt
@@ -0,0 +1,11 @@
+package eu.totorials.helloword08012024
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
